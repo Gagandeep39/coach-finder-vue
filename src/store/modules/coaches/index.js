15cab2fe459f5638bfcf09274846885c9c9de5d5
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-17 13:59:04 +0000

    Added dummy data

--- src/store/modules/coaches/index.js
+++ src/store/modules/coaches/index.js
@@ -5,7 +5,34 @@ import getters from './getters';
 export default {
   namespaced: true,
   state() {
-    return {};
+    return {
+      coaches: [
+        {
+          id: 'c1',
+          firstName: 'Gagandeep',
+          lastName: 'Singh',
+          areas: ['frontend', 'backend', 'career'],
+          description: "I'm Gagandeep Singh and I'm a freelance web developer'",
+          hourlyRate: 50,
+        },
+        {
+          id: 'c2',
+          firstName: 'Shivani',
+          lastName: 'Mane',
+          areas: ['frontend', 'career'],
+          description: 'I am Shivani. I am a Data Scientist',
+          hourlyRate: 30,
+        },
+        {
+          id: 'c3',
+          firstName: 'Pranav',
+          lastName: 'Karmarkar',
+          areas: ['frontend', 'backend', 'career'],
+          description: "I'm a Frontend developer'",
+          hourlyRate: 30,
+        },
+      ],
+    };
   },
   mutations,
   actions,
