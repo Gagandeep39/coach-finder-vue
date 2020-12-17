0ae92f03e1f89ae337fef28dcf47c44327aaa0ea
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-17 19:09:03 +0000

    Added submit logic

--- src/views/coaches/CoachRegistration.vue
+++ src/views/coaches/CoachRegistration.vue
@@ -1,9 +1,9 @@
 <template>
   <section>
-    <base-card
-      ><h2>Register as Coach</h2>
-      <coach-form
-    /></base-card>
+    <base-card>
+      <h2>Register as Coach</h2>
+      <coach-form @save-data="saveData" />
+    </base-card>
   </section>
 </template>
 <script>
@@ -11,6 +11,13 @@ import CoachForm from '../../components/coaches/CoachForm.vue';
 import BaseCard from '../../components/ui/BaseCard.vue';
 export default {
   components: { CoachForm, BaseCard },
+  methods: {
+    saveData(formData) {
+      this.$store.dispatch('coaches/registerCoach', formData);
+      // Redirect to coach route
+      this.$router.replace('/coaches');
+    },
+  },
 };
 </script>
 <style scoped></style>
