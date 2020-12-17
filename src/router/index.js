512ef6b6c8d2be75ce015ca52d352324955408a8
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-17 13:18:42 +0000

    Added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-const routes = [];
+const routes = [
+  { path: '/', redirect: '/coaches' },
+  { path: '/coaches', component: null },
+  {
+    path: '/coaches/:id',
+    component: null,
+    children: [{ path: 'contact', component: null }],
+  },
+  { path: '/register', component: null },
+  { path: '/requests', component: null },
+  // (.*) - Any value will direct to this route
+  // :notFound - Used to fetch the entered path
+  { path: '/:notFound(.*)', component: null },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
