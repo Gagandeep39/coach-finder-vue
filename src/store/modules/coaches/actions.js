1466d5d129fa579a6c3a6d8482875b1336a1ff92
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-17 19:08:44 +0000

    Added action, mutation

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,1 +1,9 @@
-export default {};
+export default {
+  registerCoach(context, payload) {
+    const coachData = {
+      ...payload,
+      id: new Date().toISOString(),
+    };
+    context.commit('registerCoach', coachData);
+  },
+};
