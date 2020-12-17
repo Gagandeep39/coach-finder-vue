85ad05ffa95267d5ede4142b5f3831022f456b90
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-17 14:04:48 +0000

    Added getters for coach

--- src/store/modules/coaches/getters.js
+++ src/store/modules/coaches/getters.js
@@ -1,1 +1,10 @@
-export default {};
+export default {
+  // Returns coach list
+  coaches(state) {
+    return state.coaches;
+  },
+  // Returns true if coaches exist
+  hasCoaches(state) {
+    return state.coaches && state.coaches.length > 0;
+  },
+};
