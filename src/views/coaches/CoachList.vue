3331fb2f744dbd30dd8d7e185bf881d77b440250
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-17 14:18:42 +0000

    - Replaced coach list with items - Added css

--- src/views/coaches/CoachList.vue
+++ src/views/coaches/CoachList.vue
@@ -6,16 +6,24 @@
       <router-link to="/register">Register as Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach"
+        :id="coach.id"
+        :firstName="coach.firstName"
+        :lastName="coach.lastName"
+        :areas="coach.areas"
+        :rate="coach.hourlyRate"
+      />
     </ul>
     <h3 v-else>No Coaches Found</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 export default {
+  components: { CoachItem },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -27,4 +35,15 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
