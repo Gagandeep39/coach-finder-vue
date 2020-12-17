7c53d7c5074860c4776defe616fb2f589e2a4206
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-17 16:52:56 +0000

    Aded coach details

--- src/views/coaches/CoachDetails.vue
+++ src/views/coaches/CoachDetails.vue
@@ -1,13 +1,67 @@
 <template>
-  <div>
-    Coach details
-    <router-link to="/coaches/t1/contact">Contact</router-link>
-  </div>
-  <router-view />
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>Coaching Price: Rs. {{ rate }}/hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now.</h2>
+        <base-button link :to="contactLink"> Contact </base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      ></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
 
 <script>
-export default {};
+import BaseBadge from '../../components/ui/BaseBadge.vue';
+import BaseCard from '../../components/ui/BaseCard.vue';
+import BaseButton from '../../components/ui/BaseButton.vue';
+export default {
+  components: { BaseCard, BaseBadge, BaseButton },
+  props: ['id'],
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+  created() {
+    this.selectedCoach = this.$store.getters['coaches/coaches'].find(
+      (coach) => coach.id === this.id
+    );
+  },
+  computed: {
+    fullName() {
+      return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
+    },
+    contactLink() {
+      return `${this.$route.path}/${this.id}/contact`;
+    },
+    areas() {
+      return this.selectedCoach.areas;
+    },
+    description() {
+      return this.selectedCoach.description;
+    },
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+  },
+};
 </script>
 
 <style></style>
