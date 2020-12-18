a4b2c0a8c32310428b6fb56d2be164d41ca0f30a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-18 12:37:04 +0000

    Added auto logout

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+let timer;
 export default {
   async login(context, payload) {
     // return is requird as we want to refer to async await of auth method
@@ -13,11 +14,13 @@ export default {
   logout(context) {
     localStorage.removeItem('token');
     localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
     context.commit('setUser', {
       token: null,
       userId: null,
-      tokenExpiration: null,
     });
+
+    clearTimeout(timer);
   },
 
   async auth(context, payload) {
@@ -38,23 +41,41 @@ export default {
       });
 
     // Persist to local storage
+    const expiresIn = +res.data.expiresIn * 1000;
+    const expirationDate = new Date().getTime() + expiresIn;
     localStorage.setItem('token', res.data.idToken);
     localStorage.setItem('userId', res.data.localId);
+    localStorage.setItem('tokenExpiration', expirationDate);
+
+    // Commit to store
     context.commit('setUser', {
       token: res.data.idToken,
       userId: res.data.localId,
-      tokenExpiration: res.data.expiresIn,
     });
+
+    // Logout on expiration
+    timer = setTimeout(() => {
+      context.dispatch('logout');
+    }, expiresIn);
   },
 
   tryLogin(context) {
     const token = localStorage.getItem('token');
     const userId = localStorage.getItem('userId');
+
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+    const expiresIn = +tokenExpiration - new Date().getTime();
+
+    // If token expired then dont login
+    if (expiresIn < 0) return;
+    timer = setTimeout(() => {
+      context.dispatch('logout');
+    }, expiresIn);
+
     if (token && userId) {
       context.commit('setUser', {
         token,
         userId,
-        tokenExpiration: null,
       });
     }
   },
