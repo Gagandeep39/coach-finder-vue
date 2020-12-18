f852a72d6e4a57ca579c6cd77295169af7cc66a1
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-18 12:20:50 +0000

    Removed duplicate code

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -2,45 +2,12 @@ import axios from 'axios';
 
 export default {
   async login(context, payload) {
-    const res = await axios
-      .post(
-        `${process.env.VUE_APP_FIREBASE_SIGN_IN_URL}${process.env.VUE_APP_FIREBASE_API_KEY}`,
-        {
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true, // Returns ID and refresh token
-        }
-      )
-      .catch((error) => {
-        console.log(error);
-        throw new Error(error.message || 'Failed to Authenticate');
-      });
-    context.commit('setUser', {
-      token: res.data.idToken,
-      userId: res.data.localId,
-      tokenExpiration: res.data.expiresIn,
-    });
+    // return is requird as we want to refer to async await of auth method
+    return context.dispatch('auth', { ...payload, mode: 'login' });
   },
 
   async signup(context, payload) {
-    const res = await axios
-      .post(
-        `${process.env.VUE_APP_FIREBASE_SIGN_UP_URL}${process.env.VUE_APP_FIREBASE_API_KEY}`,
-        {
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true, // Returns ID and refresh token
-        }
-      )
-      .catch((error) => {
-        console.log(error);
-        throw new Error(error.message || 'Failed to Authenticate');
-      });
-    context.commit('setUser', {
-      token: res.data.idToken,
-      userId: res.data.localId,
-      tokenExpiration: res.data.expiresIn,
-    });
+    return context.dispatch('auth', { ...payload, mode: 'signup' });
   },
 
   logout(context) {
@@ -48,6 +15,29 @@ export default {
       token: null,
       userId: null,
       tokenExpiration: null,
+    });
+  },
+
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url = `${process.env.VUE_APP_FIREBASE_SIGN_IN_URL}${process.env.VUE_APP_FIREBASE_API_KEY}`;
+    if (mode === 'signup')
+      url = `${process.env.VUE_APP_FIREBASE_SIGN_UP_URL}${process.env.VUE_APP_FIREBASE_API_KEY}`;
+
+    const res = await axios
+      .post(url, {
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true, // Returns ID and refresh token
+      })
+      .catch((error) => {
+        console.log(error);
+        throw new Error(error.message || 'Failed to Authenticate');
+      });
+    context.commit('setUser', {
+      token: res.data.idToken,
+      userId: res.data.localId,
+      tokenExpiration: res.data.expiresIn,
     });
   },
 };
