755ace78d96429485cb8617fef43a8a96ef88b58
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-18 06:18:24 +0000

    Fixed error handler

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -17,23 +17,24 @@ export default {
   },
   async loadCoaches(context) {
     const coaches = [];
-    const res = await axios.get(`${process.env.VUE_APP_URL}.json`);
-    if (!res.status === 200) {
-      // error
-    } else {
-      const resData = res.data;
-      for (const key in resData) {
-        const coach = {
-          firstName: resData[key].firstName,
-          lastName: resData[key].lastName,
-          description: resData[key].description,
-          hourlyRate: resData[key].hourlyRate,
-          areas: resData[key].areas,
-          id: key,
-        };
-        coaches.push(coach);
-      }
-      context.commit('setCoaches', coaches);
+    const res = await axios
+      .get(`${process.env.VUE_APP_URL}.jsons`)
+      .catch((error) => {
+        throw new Error(error.message || 'Failed to fetch');
+      });
+
+    const resData = res.data;
+    for (const key in resData) {
+      const coach = {
+        firstName: resData[key].firstName,
+        lastName: resData[key].lastName,
+        description: resData[key].description,
+        hourlyRate: resData[key].hourlyRate,
+        areas: resData[key].areas,
+        id: key,
+      };
+      coaches.push(coach);
     }
+    context.commit('setCoaches', coaches);
   },
 };
