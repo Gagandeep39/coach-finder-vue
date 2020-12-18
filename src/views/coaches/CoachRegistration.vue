3beee5b996e0bc14055f0def836478d1b5033cc9
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-18 06:35:09 +0000

    Added error dialog in register coach

--- src/views/coaches/CoachRegistration.vue
+++ src/views/coaches/CoachRegistration.vue
@@ -1,4 +1,7 @@
 <template>
+  <base-dialog :show="!!error" @close="closeDialog">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <h2>Register as Coach</h2>
@@ -9,14 +12,28 @@
 <script>
 import CoachForm from '../../components/coaches/CoachForm.vue';
 import BaseCard from '../../components/ui/BaseCard.vue';
+import BaseDialog from '../../components/ui/BaseDialog.vue';
+
 export default {
-  components: { CoachForm, BaseCard },
+  components: { CoachForm, BaseCard, BaseDialog },
   methods: {
-    saveData(formData) {
-      this.$store.dispatch('coaches/registerCoach', formData);
+    async saveData(formData) {
+      try {
+        await this.$store.dispatch('coaches/registerCoach', formData);
+        this.$router.replace('/coaches');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong';
+      }
       // Redirect to coach route
-      this.$router.replace('/coaches');
     },
+    closeDialog() {
+      this.error = null;
+    },
+  },
+  data() {
+    return {
+      error: null,
+    };
   },
 };
 </script>
