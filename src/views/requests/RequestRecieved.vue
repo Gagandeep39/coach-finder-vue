213f8deb920772303b238256688b07c26900f7d1
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-18 04:58:28 +0000

    Implemented view requests page

--- src/views/requests/RequestRecieved.vue
+++ src/views/requests/RequestRecieved.vue
@@ -1,9 +1,39 @@
 <template>
-  <div></div>
+  <section>
+    <base-card>
+      <header><h2>Requests Recieved</h2></header>
+      <ul v-if="hasRequests">
+        <li v-for="request in requests" :key="request">cvf</li>
+      </ul>
+      <h3 v-else>No Requests have been recieved</h3>
+    </base-card>
+  </section>
 </template>
 
 <script>
-export default {};
+import BaseCard from '../../components/ui/BaseCard.vue';
+import { mapGetters } from 'vuex';
+export default {
+  components: { BaseCard },
+  computed: {
+    ...mapGetters('requests', ['hasRequests', 'requests']),
+  },
+};
 </script>
 
-<style></style>
+<style>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
