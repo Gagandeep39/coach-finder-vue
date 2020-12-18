a3f266c0fc6efdd48a9ec96f6d942431958b66b8
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-18 05:15:40 +0000

    Fixed improper contact navigation

--- src/views/coaches/CoachDetails.vue
+++ src/views/coaches/CoachDetails.vue
@@ -9,7 +9,13 @@
     <base-card>
       <header>
         <h2>Interested? Reach out now.</h2>
-        <base-button link :to="contactLink"> Contact </base-button>
+        <base-button
+          link
+          :to="contactLink"
+          v-if="!$route.path.includes('contact')"
+        >
+          Contact
+        </base-button>
       </header>
       <router-view></router-view>
     </base-card>
