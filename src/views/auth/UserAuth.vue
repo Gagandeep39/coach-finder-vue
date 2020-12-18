bd9c8d00aa7ce213731e12b24d32bd42c29e75c0
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-18 11:03:42 +0000

    Aded loading, error message

--- src/views/auth/UserAuth.vue
+++ src/views/auth/UserAuth.vue
@@ -1,40 +1,53 @@
 <template>
-  <base-card>
-    <form @submit.prevent="submitForm">
-      <div class="form-control">
-        <label for="email">Email</label>
-        <input type="email" id="email" v-model.trim="email" />
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model.trim="password" />
-      </div>
-      <p v-if="!formIsValid" class="errors">
-        Enter valid data. Password must be atleast 6 characters
-      </p>
-      <base-button> {{ submitButtonCaption }} </base-button>
-      <base-button type="button" mode="flat" @click="switchFormMode">
-        {{ switchButtonCaption }}
-      </base-button>
-    </form>
-  </base-card>
+  <div>
+    <base-dialog :show="!!error" @close="closeDialog">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <base-dialog :show="isLoading" title="Authenticating..." fixed>
+      <p>Authenticating...</p>
+      <base-spinner />
+    </base-dialog>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">Email</label>
+          <input type="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password" />
+        </div>
+        <p v-if="!formIsValid" class="errors">
+          Enter valid data. Password must be atleast 6 characters
+        </p>
+        <base-button> {{ submitButtonCaption }} </base-button>
+        <base-button type="button" mode="flat" @click="switchFormMode">
+          {{ switchButtonCaption }}
+        </base-button>
+      </form>
+    </base-card>
+  </div>
 </template>
 <script>
 import BaseButton from '../../components/ui/BaseButton.vue';
 import BaseCard from '../../components/ui/BaseCard.vue';
+import BaseDialog from '../../components/ui/BaseDialog.vue';
+import BaseSpinner from '../../components/ui/BaseSpinner.vue';
 
 export default {
-  components: { BaseButton, BaseCard },
+  components: { BaseButton, BaseCard, BaseDialog, BaseSpinner },
   data() {
     return {
       email: '',
       password: '',
       formIsValid: true,
       formMode: 'login',
+      isLoading: false,
+      error: null,
     };
   },
   methods: {
-    submitForm() {
+    async submitForm() {
       this.formIsValid = true;
       if (
         this.email === '' ||
@@ -44,19 +57,28 @@ export default {
         this.formIsValid = false;
         return;
       }
+      this.isLoading = true;
 
-      if (this.formMode === 'login') {
-        //..
-      } else {
-        this.$store.dispatch('signup', {
-          email: this.email,
-          password: this.password,
-        });
+      try {
+        if (this.formMode === 'login') {
+          //..
+        } else {
+          await this.$store.dispatch('signup', {
+            email: this.email,
+            password: this.password,
+          });
+        }
+      } catch (error) {
+        this.error = error.message || 'Something went wrong';
       }
+      this.isLoading = false;
     },
     switchFormMode() {
       if (this.formMode === 'login') this.formMode = 'register';
       else this.formMode = 'login';
+    },
+    closeDialog() {
+      this.error = null;
     },
   },
   computed: {
