29ff0b52955508da981d056c796cda7dcd6779f6
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-18 12:14:38 +0000

    Adde auth guard

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import ContactCoach from '../views/requests/ContactCoach';
 import RequestRecieved from '../views/requests/RequestRecieved';
 import UserAuth from '../views/auth/UserAuth';
 import NotFound from '../views/NotFound';
+import store from '../store';
 
 const routes = [
   { path: '/', redirect: '/coaches' },
@@ -16,9 +17,17 @@ const routes = [
     props: true,
     children: [{ path: 'contact', component: ContactCoach }],
   },
-  { path: '/register', component: CoachRegistration },
-  { path: '/requests', component: RequestRecieved },
-  { path: '/auth', component: UserAuth },
+  {
+    path: '/register',
+    component: CoachRegistration,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/requests',
+    component: RequestRecieved,
+    meta: { requiresAuth: true },
+  },
+  { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
   // (.*) - Any value will direct to this route
   // :notFound - Used to fetch the entered path
   { path: '/:notFound(.*)', component: NotFound },
@@ -27,6 +36,16 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  // User acess protected route and is not logged in
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) next('/auth');
+  // User access login page, but is already logged in
+  else if (to.meta.requiresUnauth && store.getters.isAuthenticated)
+    next('/coaches');
+  // All routing
+  else next();
 });
 
 export default router;
