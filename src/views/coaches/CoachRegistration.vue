bc064b7dbce22f1edf37137c06f37167af3602af
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-18 06:42:46 +0000

    Added loading in register form

--- src/views/coaches/CoachRegistration.vue
+++ src/views/coaches/CoachRegistration.vue
@@ -4,8 +4,15 @@
   </base-dialog>
   <section>
     <base-card>
-      <h2>Register as Coach</h2>
-      <coach-form @save-data="saveData" />
+      <keep-alive>
+        <div v-if="isLoading">
+          <base-spinner />
+        </div>
+        <div v-else>
+          <h2>Register as Coach</h2>
+          <coach-form @save-data="saveData" />
+        </div>
+      </keep-alive>
     </base-card>
   </section>
 </template>
@@ -13,17 +20,20 @@
 import CoachForm from '../../components/coaches/CoachForm.vue';
 import BaseCard from '../../components/ui/BaseCard.vue';
 import BaseDialog from '../../components/ui/BaseDialog.vue';
+import BaseSpinner from '../../components/ui/BaseSpinner.vue';
 
 export default {
-  components: { CoachForm, BaseCard, BaseDialog },
+  components: { CoachForm, BaseCard, BaseDialog, BaseSpinner },
   methods: {
     async saveData(formData) {
       try {
+        this.isLoading = true;
         await this.$store.dispatch('coaches/registerCoach', formData);
         this.$router.replace('/coaches');
       } catch (error) {
         this.error = error.message || 'Something went wrong';
       }
+      this.isLoading = false;
       // Redirect to coach route
     },
     closeDialog() {
@@ -32,6 +42,7 @@ export default {
   },
   data() {
     return {
+      isLoading: false,
       error: null,
     };
   },
