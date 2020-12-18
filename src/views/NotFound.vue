b538b4accdfb54655b19486ffd301ae48f24e703
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-18 07:56:56 +0000

    Added not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,18 @@
 <template>
-  <div></div>
+  <section>
+    <base-card>
+      <h2>Page Not Found</h2>
+      <p>
+        This page could not be found. Go back to
+        <router-link to="/">home</router-link> page
+      </p>
+    </base-card>
+  </section>
 </template>
 <script>
-export default {};
+import BaseCard from '../components/ui/BaseCard.vue';
+export default {
+  components: { BaseCard },
+};
 </script>
 <style scoped></style>
